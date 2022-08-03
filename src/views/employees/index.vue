18b3e730ad0be56a00c0107b4012cc474fff9632
Yyscy <2103374630@qq.com>
2022-08-03 16:09:02 +0800

    路由框架的搭建

---
+++ src/views/employees/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>员工</div>
+</template>
+
+<script>
+export default {
+
+  // 组件
+  components: {
+
+  },
+  // 数据
+  data() {
+    return {
+
+    }
+  },
+
+  // 计算属性
+  computed: {
+
+  },
+
+  // 创建后
+  created() {
+
+  },
+
+  // 函数
+  methods: {
+
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
